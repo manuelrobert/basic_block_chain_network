0d9582b48bb7d313977ebd682746eedab26d471b
Manuel Robert <manuelrobz@outlook.com>
2020-12-03 00:20:04 +0530

    first commit

---
+++ createNetwork.sh
@@ -0,0 +1,93 @@
+export FABRIC_CFG_PATH=${PWD}/configtx
+export VERBOSE=false
+source scriptUtils.sh
+
+IMAGETAG="latest"
+COMPOSE_FILE_BASE=docker/docker-compose-test-net.yaml
+COMPOSE_FILE_COUCH=docker/docker-compose-couch.yaml
+
+function createOrgs() {
+    if [ -d "organizations/peerOrganizations" ]; then
+        rm -Rf organizations/peerOrganizations && rm -Rf organizations/ordererOrganizations
+    fi
+    which cryptogen
+    if [ "$?" -ne 0 ]; then
+        fatalln "Cryptogen tool not found. exiting..."
+    fi
+    infoln "Generating certificates using cryptogen tool"
+
+    infoln "Creating Org1 Identities"
+    set -x
+    cryptogen generate --config=./organizations/cryptogen/crypto-config-org1.yaml --output="organizations"
+    res=$?
+    { set +x; } 2>/dev/null
+    if [ $res -ne 0 ]; then
+      fatalln "Failed to generate certificates..."
+    fi
+
+    infoln "Creating Org2 Identities"
+    set -x
+    cryptogen generate --config=./organizations/cryptogen/crypto-config-org2.yaml --output="organizations"
+    res=$?
+    { set +x; } 2>/dev/null
+    if [ $res -ne 0 ]; then
+      fatalln "Failed to generate certificates..."
+    fi
+
+    infoln "Creating Orderer Org Identities"
+    set -x
+    cryptogen generate --config=./organizations/cryptogen/crypto-config-orderer.yaml --output="organizations"
+    res=$?
+    { set +x; } 2>/dev/null
+    if [ $res -ne 0 ]; then
+      fatalln "Failed to generate certificates..."
+    fi
+
+    infoln "Generating CCP files for Org1 and Org2"
+    ./organizations/ccp-generate.sh
+}
+
+# Generate orderer system channel genesis block.
+function createConsortium() {
+
+  which configtxgen
+  if [ "$?" -ne 0 ]; then
+    fatalln "configtxgen tool not found."
+  fi
+
+  infoln "Generating Orderer Genesis block"
+
+  # Note: For some unknown reason (at least for now) the block file can't be
+  # named orderer.genesis.block or the orderer will fail to launch!
+  set -x
+  configtxgen -profile TwoOrgsOrdererGenesis -channelID system-channel -outputBlock ./system-genesis-block/genesis.block
+  res=$?
+  { set +x; } 2>/dev/null
+  if [ $res -ne 0 ]; then
+    fatalln "Failed to generate orderer genesis block..."
+  fi
+}
+
+# Bring up the peer and orderer nodes using docker compose.
+function networkUp() {
+
+  # generate artifacts if they don't exist
+  if [ ! -d "organizations/peerOrganizations" ]; then
+    createOrgs
+    createConsortium
+  fi
+
+  COMPOSE_FILES="-f ${COMPOSE_FILE_BASE}"
+
+  if [ "${DATABASE}" == "couchdb" ]; then
+    COMPOSE_FILES="${COMPOSE_FILES} -f ${COMPOSE_FILE_COUCH}"
+  fi
+
+  IMAGE_TAG=$IMAGETAG docker-compose ${COMPOSE_FILES} up -d 2>&1
+
+  docker ps -a
+  if [ $? -ne 0 ]; then
+    fatalln "Unable to start network"
+  fi
+}
+networkUp #starting the network
